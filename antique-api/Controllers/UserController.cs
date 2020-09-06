9f31f32b03d8332cf00a97f805bb30d972633f59
zanuda <zaanudaa@gmail.com>
2020-09-06 12:35:50 +0300

    i forgot what i changed

--- antique-api/Controllers/UserController.cs
+++ antique-api/Controllers/UserController.cs
@@ -2,6 +2,7 @@
 using antique_api.Services;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Cors;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 using System;
 using System.Collections.Generic;
